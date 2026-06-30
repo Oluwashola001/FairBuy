05a21d105d3996718a051eabc3c91a62b4f5d3c4
Oluwashola001 <oyagbinrinoluwashola@gmail.com>
2026-06-30 07:58:23 +0100

    fix: lib env

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,16 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
+import 'react-native-url-polyfill/auto';
 
-const supabaseUrl = 'https://rllvgjmyjlcebbcvywql.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJsbHZnam15amxjZWJiY3Z5d3FsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIwMjYwNjIsImV4cCI6MjA4NzYwMjA2Mn0.BIsVX2Nth4FJ9Z-fhyNKwgJ9K8nFwnZfhbItHOTs7as';
+// Pulling the keys securely from the .env file
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL as string;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY as string;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+})
